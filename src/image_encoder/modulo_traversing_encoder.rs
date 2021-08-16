459f99f3ab5fca122fc26fb04c6a4d0ed74fb0af
Paweł Górski <dev.baymax42@gmail.com>
2021-08-17 00:08:42 +0200

    Removed unneeded code from modulo encoder

--- src/image_encoder/modulo_traversing_encoder.rs
+++ src/image_encoder/modulo_traversing_encoder.rs
@@ -5,10 +5,6 @@ pub struct ModuloTraversingEncoder;
 // Original implementation - works only for square images
 impl ImageEncoder for ModuloTraversingEncoder {
     fn get_next_pixel_pos(&self, (x, y): (u32, u32), (w, h): (u32, u32), index: usize) -> Option<(u32, u32)> {
-        // if index / 8 >= (w * h) as usize {
-        //     return None;
-        // }
-
         let mut x_pos = x;
         let mut y_pos = y;
         if index % w as usize == 0 {
